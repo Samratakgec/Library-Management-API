fb9f6126eff57cdc3392972a58571e40d72a2db7
samrat/akgec <palnutsam2002@gmail.com>
2025-06-09 12:20:07 +0530

    adding some comments in models/entities.go

--- models/entities.go
+++ models/entities.go
@@ -4,12 +4,14 @@ import (
 	"time"
 )
 
+// defining book model as entity
 type Book struct {
 	ISBN   string `json:"isbn" bson:"isbn"`
 	Name   string `json:"name" bson:"name"`
 	Author string `json:"author" bson:"author"`
 }
 
+// defining TransactionLogs model as entity
 type TransactionLogs struct {
 	TimestampIn    time.Time `json:"timestamp_in" bson:"timestamp_in"`
 	TimestampOut   time.Time `json:"timestamp_out" bson:"timestamp_out"`
@@ -19,7 +21,7 @@ type TransactionLogs struct {
 	IsReturned	   bool		 `json:"is_returned" bson:"is_returned"`
 }
 
-
+// defining User model as entity
 type User struct {
 	EmpID    string             `bson:"emp_id,omitempty"`  // For librarian
 	StdID    string             `bson:"std_id,omitempty"`  // For student
